94dd231b415708f70e344d8361031006d96e160a
furucrm-thanh-tran <67886986+furucrm-thanh-tran@users.noreply.github.com>
2020-07-22 14:44:52 +0700

    create migrate

---
+++ database/migrations/2020_07_22_063837_create_feedback_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+
+            //foreignKey
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('cus_id')
+                ->constrained('customers')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
