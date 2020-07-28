6f85fbc2dd5b5dc8b214e1288c186649b39cac91
furucrm-thanh-tran <67886986+furucrm-thanh-tran@users.noreply.github.com>
2020-07-28 09:26:37 +0700

    edit route, create AdminController

--- routes/web.php
+++ routes/web.php
@@ -23,32 +23,44 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 // Manager routes
-Route::prefix('manager')->group(function(){
+Route::prefix('manager')->group(function () {
     Route::get('/', 'ManagerController@index')->name('seller.dashboard');
     Route::get('/login', 'Auth\ManagerLoginController@showLoginForm')->name('manager.login');
     Route::post('/login', 'Auth\ManagerLoginController@login')->name('manager.login.submit');
 });
 
-Route::get('admin', 'ManagerController@adminHome')->name('admin')->middleware('role');
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'ManagerController@adminHome')->name('admin')->middleware('role');
+    Route::get('/sellermanager', 'AdminController@sellerManager')->name('admin.sellermanager')->middleware('role');
+    Route::get('/transactionmanager', 'AdminController@transactionManager')->name('admin.transactionmanager')->middleware('role');
+    Route::get('/productmanager', 'AdminController@productManager')->name('admin.productmanager')->middleware('role');
+    Route::get('/license-key', 'AdminController@licenseKey')->name('admin.license-key')->middleware('role');
+});
+
+// Route::get('admin', 'ManagerController@adminHome')->name('admin')->middleware('role');
+// Route::get('/admin/sellermanager', 'AdminController@sellerManager')->name('admin.sellermanager')->middleware('role');
+// Route::get('/admin/transactionmanager', 'AdminController@transactionManager')->name('admin.transactionmanager')->middleware('role');
+// Route::get('/admin/productmanager', 'AdminController@productManager')->name('admin.productmanager')->middleware('role');
+// Route::get('/admin/license-key', 'AdminController@licenseKey')->name('admin.license-key')->middleware('role');
+
 
 
 // Route::get('admin', function (){
 //     return view('admin.index');
 // })->name('admin');
 
-Route::get('/admin/sellermanager', function (){
-    return view('admin.seller_manager');
-})->name('admin.sellermanager');
+// Route::get('/admin/sellermanager', function (){
+//     return view('admin.seller_manager');
+// })->name('admin.sellermanager');
 
-Route::get('/admin/transactionmanager', function (){
-    return view('admin.transaction_manager');
-})->name('admin.transactionmanager');
+// Route::get('/admin/transactionmanager', function (){
+//     return view('admin.transaction_manager');
+// })->name('admin.transactionmanager');
 
-Route::get('/admin/productmanager', function (){
-    return view('admin.product_manager');
-})->name('admin.productmanager');
+// Route::get('/admin/productmanager', function (){
+//     return view('admin.product_manager');
+// })->name('admin.productmanager');
 
-Route::get('/admin/license-key', function (){
-    return view('admin.license-key');
-})->name('admin.license-key');
-
+// Route::get('/admin/license-key', function (){
+//     return view('admin.license-key');
+// })->name('admin.license-key');
