a005e8477aab1630a1d643ab02a9b40afe8cb0a7
TruongNgo98 <truongngo1998@gmail.com>
2020-08-04 12:07:13 +0700

    update model

--- app/Bill.php
+++ app/Bill.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bill extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function managers()
+    {
+        return $this->belongsTo('App\Manager', 'seller_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Product', 'bill__products', 'bill_id', 'pro_id');
+    }
+
+    public function bill__products()
+    {
+        return $this->hasMany('App\Bill_Product', 'bill_id');
+    }
 }
