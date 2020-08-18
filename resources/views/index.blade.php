3d44ce4e59a4a2cfd73672959c5b713abd8c1a1c
TruongNgo98 <truongngo1998@gmail.com>
2020-08-18 12:51:19 +0700

    added sort and search product in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,52 +30,65 @@
 @section('title') License Store @endsection
 @section('content')
     <x-product-detail />
-    <div class="row">
-        <div class="col-md-3">
-            <div class="osahan-account-page-left shadow-sm bg-white h-100">
-                <a class="nav-link active " type="button" onclick="sortListNew()" id="new">New</a>
-                <a class="nav-link active" type="button" onclick="sortListUpdate()" id="update">Update</a>
-                <a class="nav-link active" type="button" onclick="sortListView()" id="view">View</a>
-                <a class="nav-link active" type="button" onclick="sortListBuy()" id="buy">Buy</a>
-            </div>
+    <div class="form-inline bg-white p-3 mb-3">
+        <div class="form-inline col-sm-6">
+            <span>Sort by:</span>
+            <select class="form-control w-auto ml-2" name="" id="" onchange="sortList(value);" value="1">
+                <option value="1" selected disabled hidden>Select...</option>
+                <option value="new.date">New</option>
+                <option value="update.date">Update</option>
+                <option value="view">View</option>
+                <option value="buy">Buy</option>
+            </select>
         </div>
-        <div class="col-md-9">
-            <div class="row list_pro">
-                @foreach ($product as $p)
-                    <div data-view="{{ $p->view }}" data-buy="{{ $p->buy }}" data-new="{{ $p->created_at }}" data-update="{{ $p->updated_at }}"
-                        class="sort col-md-3 mt-3">
 
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ $p->icon_pro }}" alt="" class="img-fluid">
-                                <div class="caption">
-                                    <h5 class="pull-right">${{ $p->price_license }}</h5>
-                                    <a href="#">{{ $p->name_pro }}</a>
-                                </div>
+        <div class="form-group col-sm-6 my-2 justify-content-end">
+            <input type="search" id="search" onkeyup="filtedList()" value="" class="form-control"
+                placeholder="Search name...">
+        </div>
+    </div>
+    <div class="row list_pro">
+        @foreach ($product as $p)
+            <div data-name="{{ $p->name_pro }}" data-view="{{ $p->view }}" data-buy="{{ $p->buy }}"
+                data-new="{{ $p->created_at }}" data-update="{{ $p->updated_at }}" class="sort col-lg-4 mb-3">
 
-                                <div class="space-ten">
-                                    <a>View: </a><a id="{{ $p->id }}">{{ $p->view }}</a><br>
-                                    <a>New: </a><a>{{ $p->created_at }}</a><br>
-                                    <a>Update: </a><a>{{ $p->updated_at }}</a><br>
-                                    <a>Buy: </a><a>{{ $p->buy }}</a>
+                <div class="card">
+                    <div class="card-body">
+                        <img src="{{ $p->icon_pro }}" alt="" class="mx-auto d-block img-thumbnail img-fluid">
+                        <div class="caption mt-3">
+                            <h5 class="pull-right">${{ $p->price_license }}</h5>
+                            <h4>{{ $p->name_pro }}</h6>
+                                <div class="mt-3">
+                                    <div class="d-flex flex-nowrap">
+                                        <div><i class="fa fa-eye"></i> <span id="{{ $p->id }}">{{ $p->view }}</span></div>
+                                        <div class="ml-3"><span><i class="fas fa-user-tag"></i>
+                                                @if ($p->buy)
+                                                    {{ $p->buy }}
+                                                @else
+                                                    0
+                                                @endif
+                                                purchase
+                                            </span></div>
+                                    </div>
+
+                                    <div>Created: </a><a>{{ $p->created_at }}</div>
+                                    <div>Updated: </a><a>{{ $p->updated_at }}</div>
                                 </div>
-                                <div class="btn-ground text-center">
-                                    <a href="/login" class="add_to_card btn btn-primary"><i
-                                            class="fa fa-shopping-cart"></i></a>
-                                    <button name="{{ $p->id }}" class="btn btn-primary btn-view" data-toggle="modal"
-                                        data-target="#viewProduct" data-view="{{ $p->view }}" data-id="{{ $p->id }}"
-                                        data-name_pro="{{ $p->name_pro }}" data-description_pro="{{ $p->description_pro }}"
-                                        data-icon_pro="{{ $p->icon_pro }}" data-price_license="{{ $p->price_license }}">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </div>
-                            </div>
                         </div>
-
+                        <div class="mt-3 text-center">
+                            <a href="/login" class="add_to_card btn btn-primary"><i class="fa fa-shopping-cart"></i></a>
+                            <button name="{{ $p->id }}" class="btn btn-primary btn-view" data-toggle="modal"
+                                data-target="#viewProduct" data-view="{{ $p->view }}" data-id="{{ $p->id }}"
+                                data-name_pro="{{ $p->name_pro }}" data-description_pro="{{ $p->description_pro }}"
+                                data-icon_pro="{{ $p->icon_pro }}" data-price_license="{{ $p->price_license }}">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
                     </div>
-                @endforeach
+                </div>
+
             </div>
-        </div>
+        @endforeach
     </div>
 @endsection
 
@@ -117,70 +130,28 @@
     </script>
 
     <script>
-        function sortListView() {
+        function sortList(col) {
+            colv = col.split(".");
             var $wrapper = $('.list_pro');
             $wrapper.find('.sort').sort(function(a, b) {
-                    return +b.dataset.view - +a.dataset.view;
+                    if (col.indexOf(".") < 0) {
+                        console.log(b.dataset[col])
+                        return +b.dataset[col] - +a.dataset[col];
+                    } else {
+                        // console.log(new Date(b.dataset['update']));
+                        return new Date(b.dataset[colv[0]]) - new Date(a.dataset[colv[0]])
+
+                    }
                 })
                 .appendTo($wrapper);
-            var view = $("#view");
-            view.addClass("bg-primary text-white");
-            var buy = $("#buy");
-            buy.removeClass("bg-primary text-white");
-            var newdate = $("#new");
-            newdate.removeClass("bg-primary text-white");
-            var updatedate = $("#update");
-            updatedate.removeClass("bg-primary text-white");
         }
 
-        function sortListBuy() {
-            var $wrapper = $('.list_pro');
-            $wrapper.find('.sort').sort(function(a, b) {
-                    return +b.dataset.buy - +a.dataset.buy;
-                })
-                .appendTo($wrapper);
-            var buy = $("#buy");
-            buy.addClass("bg-primary text-white");
-            var view = $("#view");
-            view.removeClass("bg-primary text-white");
-            var newdate = $("#new");
-            newdate.removeClass("bg-primary text-white");
-            var updatedate = $("#update");
-            updatedate.removeClass("bg-primary text-white");
-        }
-
-        function sortListNew() {
-            var $wrapper = $('.list_pro');
-            $wrapper.find('.sort').sort(function(a, b) {
-                return new Date(b.dataset.new) - new Date(a.dataset.new);
-            })
-            .appendTo($wrapper);
-
-            var newdate = $("#new");
-            newdate.addClass("bg-primary text-white");
-            var buy = $("#buy");
-            buy.removeClass("bg-primary text-white");
-            var view = $("#view");
-            view.removeClass("bg-primary text-white");
-            var updatedate = $("#update");
-            updatedate.removeClass("bg-primary text-white");
-        }
-        function sortListUpdate() {
-            var $wrapper = $('.list_pro');
-            $wrapper.find('.sort').sort(function(a, b) {
-                return new Date(b.dataset.update) - new Date(a.dataset.update);
-            })
-            .appendTo($wrapper);
-
-            var updatedate = $("#update");
-            updatedate.addClass("bg-primary text-white");
-            var newdate = $("#new");
-            newdate.removeClass("bg-primary text-white");
-            var buy = $("#buy");
-            buy.removeClass("bg-primary text-white");
-            var view = $("#view");
-            view.removeClass("bg-primary text-white");
-        }
+        function filtedList() {
+            var value = $("#search").val().toLowerCase();
+            $(".list_pro .sort").filter(function() {
+                $(this).toggle($(this).data("name").toLowerCase().indexOf(value) > -1)
+            });
+        };
 
     </script>
 
